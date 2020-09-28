825fee6e259b7d19367b4f42acbe3a75ccafca19
Martin Ahindura <martinahindura@gmail.com>
2020-09-28 23:47:00 +0300

    Add the quotes for all products by day session service

--- app/services/tokyo_commodities_exchange/file_download_site_to_db/__init__.py
+++ app/services/tokyo_commodities_exchange/file_download_site_to_db/__init__.py
@@ -1,7 +1,9 @@
 """Module containing all the controllers for the file_download_site_to_db nomics service"""
 
 from .quotes_by_trade_date import ControllerForQuotesByTradeDate
+from .quotes_by_day_session import ControllerForQuotesByDaySession
 
 TOKYO_C_E_FILE_DOWNLOAD_SITE_TO_DB_CONTROLLERS = [
     ControllerForQuotesByTradeDate,
+    ControllerForQuotesByDaySession,
 ]
