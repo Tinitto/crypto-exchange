aaaf3f9edbf3736a946413ac25c8914bad85adf4
Martin Ahindura <martinahindura@gmail.com>
2020-12-23 02:27:22 +0300

    Add Nomics currencies service

--- app/services/nomics/rest_api_to_db/__init__.py
+++ app/services/nomics/rest_api_to_db/__init__.py
@@ -1,5 +1,8 @@
 """Module containing all the controllers for the rest_api_to_db nomics service"""
 
+from .currencies.controller import ControllerForCurrencies
+
 
 NOMICS_REST_API_TO_DB_CONTROLLERS = [
+    ControllerForCurrencies,
 ]
---
+++ app/services/nomics/rest_api_to_db/currencies/controller.py
@@ -0,0 +1,20 @@
+"""Controller for getting all currencies supported by Nomics"""
+from typing import Type, List
+
+from judah.destinations.database.model import DatabaseBaseModel
+from judah.transformers.base import BaseTransformer
+
+from .destination.model import Currencies
+from .source import CurrenciesDataset
+from ..abstract.controllers.bulk import NomicsBulkRestAPIToDatabaseController
+from ..abstract.sources.bulk_source import NomicsBulkRestApiSource
+
+
+class ControllerForCurrencies(NomicsBulkRestAPIToDatabaseController):
+    """
+    The controller for getting all supported currencies from Nomics
+    """
+    destination_model_class: Type[DatabaseBaseModel] = Currencies
+    source_class: Type[NomicsBulkRestApiSource] = CurrenciesDataset
+    interval_in_milliseconds: int = 24 * 60 * 60 * 1000  # 1 day
+    transformer_classes: List[Type[BaseTransformer]] = []
